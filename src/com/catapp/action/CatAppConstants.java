c3435bce6c79ecae1adeb70388715fface12c4f4
sumit0704 <sumit07@tamu.edu>
2017-08-22 11:21:29 -0500

    Add files via upload

---
+++ src/com/catapp/action/CatAppConstants.java
@@ -0,0 +1,34 @@
+package com.catapp.action;
+
+public class CatAppConstants {
+	
+	public static final int catapp_chemicals=1;
+	public static final int assay_specific_controls=2;
+	public static final int vehicle=3;
+	public static final int reference_stds=4;
+	public static final int inter_plate_replicates=5;
+	public static final int intra_plate_replicates=6;
+	public static final int dmso_pure=7;
+	public static final int media=8;
+	public static final int dmso=9;
+	
+	
+	/// These positive controls are for cardiomyoctes ///
+	public static final int positive_control_INN=1;
+	public static final int positive_control_proparanol=2;
+	public static final int positive_control_cisapride=3;
+	public static final int positive_control_sotalol=4;
+	public static final int positive_control_tab=5;
+	
+	public static final int row_type_total=2;
+	public static final int row_type_solo=1;
+	public static final int column_type_total=3;
+	public static final String operator1=">";
+	public static final String operator2="<";
+	public static final String operator3="=";
+	
+	public static final String HOME="home";
+	public static final String ABOUT="about";
+	public static final String CONTACT="contact";
+
+}
